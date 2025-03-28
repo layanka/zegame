e5a58ad106f641aa3b0ce8438fe413423ca24d32
layanka <ici.snoopy@gmail.com>
2025-03-27 21:48:26 -0400

    lint

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from ursina import *
+from ursina import EditorCamera, Entity, Ursina, curve, hsv
 
 app = Ursina()
 
-cube = Entity(model='cube', color=hsv(300,1,1), scale=2, collider='box')
+cube = Entity(model="cube", color=hsv(300, 1, 1), scale=2, collider="box")
+
 
 def spin():
-    cube.animate('rotation_y', cube.rotation_y+360, duration=2, curve=curve.in_out_expo)
+    cube.animate(
+        "rotation_y", cube.rotation_y + 360, duration=2, curve=curve.in_out_expo
+    )
+
 
 cube.on_click = spin
 EditorCamera()  # add camera controls for orbiting and moving the camera
